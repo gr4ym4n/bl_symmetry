be5c245a6761058e3958d9f710feeb3f56f2ec43
James Snowden <jamesvsnowden@me.com>
2022-01-30 19:41:53 +0100

    Add debug

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 
 import typing
+import sys
 
-PREFIX = 0
-SUFFIX = 1
+DEBUG = 'DEBUG_MODE' is sys.argv
 
 SYMMETRY_AFIX_SEPARATORS = (".", " ", "-", "_")
 SYMMETRY_AFIXES_LEFT: typing.Set[str] = {"l", "L", "left", "Left", "LEFT"}
@@ -34,6 +34,9 @@ def is_symmetrical(name: str) -> bool:
     """
     Check whether a data block name is formatted as symmetrical.
     """
+    if DEBUG:
+        assert isinstance(name, str), (f'is_symmetrical(name): '
+                                       f'Expected name to be str, not {name.__class__.__name__}')
     return any(map(name.endswith, SYMMETRY_PREFIX_LUT)) or any(map(name.startswith, SYMMETRY_SUFFIX_LUT))
 
 def symmetrical_prefix(name: str) -> str:
@@ -41,6 +44,9 @@ def symmetrical_prefix(name: str) -> str:
     Returns the prefix for a prefixed symmetrical data block name, 
     or an empty string if the data block name is not symmetrical.
     """
+    if DEBUG:
+        assert isinstance(name, str), (f'symmetrical_prefix(name): '
+                                       f'Expected name to be str, not {name.__class__.__name__}')
     return next((afix for afix in SYMMETRY_SUFFIX_LUT if name.startswith(afix)), "")
 
 def symmetrical_suffix(name: str) -> str:
@@ -48,6 +54,9 @@ def symmetrical_suffix(name: str) -> str:
     Returns the suffix for a suffixed symmetrical data block name, 
     or an empty string if the data block name is not symmetrical.
     """
+    if DEBUG:
+        assert isinstance(name, str), (f'symmetrical_suffix(name): '
+                                       f'Expected name to be str, not {name.__class__.__name__}')
     return next((afix for afix in SYMMETRY_PREFIX_LUT if name.endswith(afix)), "")
 
 def symmatrical_afix(name: str) -> str:
@@ -55,12 +64,18 @@ def symmatrical_afix(name: str) -> str:
     Returns the afix for the symmetrical data block name,
     or an empty string if the data block is not symmetrical.
     """
+    if DEBUG:
+        assert isinstance(name, str), (f'symmetrical_afix(name): '
+                                       f'Expected name to be str, not {name.__class__.__name__}')
     return symmetrical_suffix(name) or symmetrical_prefix(name)
 
 def symmetrical_basename(name: str) -> str:
     """
     Returns the name of the data block removing the symmetrical afix
     """
+    if DEBUG:
+        assert isinstance(name, str), (f'symmetrical_basename(name): '
+                                       f'Expected name to be str, not {name.__class__.__name__}')
     afix = symmetrical_suffix(name)
     if afix:
         return f'{name[:-len(afix)]}'
@@ -73,6 +88,9 @@ def symmetrical_split(name: str) -> typing.Tuple[str, str, str]:
     """
     Splits the data block name into its symmetrical preifx, base name and symmetrical suffix.
     """
+    if DEBUG:
+        assert isinstance(name, str), (f'symmetrical_split(name): '
+                                       f'Expected name to be str, not {name.__class__.__name__}')
     afix = symmetrical_suffix(name)
     if afix:
         return "", name[:-len(afix)], afix
@@ -86,6 +104,9 @@ def symmetrical_target(name: str) -> str:
     Returns the name of the symmetrical data block if the name is symmetrical,
     otherwise returns an emptpy string.
     """
+    if DEBUG:
+        assert isinstance(name, str), (f'symmetrical_target(name): '
+                                       f'Expected name to be str, not {name.__class__.__name__}')
     afix = symmetrical_suffix(name)
     if afix:
         return f'{name[:-len(afix)]}{SYMMETRY_PREFIX_LUT[afix]}'
